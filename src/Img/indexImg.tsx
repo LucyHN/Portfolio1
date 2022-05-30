3daf304798954dd31337718f96a3f500369f80ce
Lucy Nam <lucy.h.nam@gmail.com>
2022-05-29 21:25:30 -0300

    First commit

---
+++ src/Img/indexImg.tsx
@@ -0,0 +1,8 @@
+const Images = {
+    careerImg_1: require('./career_1.JPG'),
+    careerImg_2: require('./career_2.JPG'),
+    careerImg_3: require('./career_3.JPG'),
+
+}
+
+export default Images;
---
+++ src/Routes/About.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import {AnimatePresence, motion} from "framer-motion";
+import Images from "../Img/indexImg";
+import { useState } from "react";
+
+const Wrapper = styled.div `
+    display: flex;
+    height: 120vh;
+    align-items: center;
+    flex-direction: column;
+`;
+
+function About() {
+    return (
+        <Wrapper>
+            
+        </Wrapper>
+        );
+    }  
+
+export default About;
